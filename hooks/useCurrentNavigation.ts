1cdbb9f4c2910fa8bd3cea2f8539d767cdd247f4
shahsawar92 <shahsawar92@gmail.com>
2025-01-20 17:45:34 +0500

    config

--- hooks/useCurrentNavigation.ts
+++ hooks/useCurrentNavigation.ts
@@ -1,17 +1,17 @@
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
-import logger from '@/lib/logger';
+import logger from "@/lib/logger";
 
-import { navigationItems } from '@/components/dashboard/Sidebar/navigation-items';
+import { navigationItems } from "@/components/dashboard/Sidebar/navigation-items";
 
 export function useCurrentNavigation() {
   const pathname = usePathname();
 
   const currentItem = navigationItems
-    .filter((item) => pathname.startsWith(item.path)) // Filter matching paths
-    .sort((a, b) => b.path.length - a.path.length) // Prioritize deeper paths
-    .shift(); // Get the first match
+    .filter((item) => pathname.startsWith(item.path))
+    .sort((a, b) => b.path.length - a.path.length)
+    .shift();
 
-  logger(currentItem, 'currentItem');
-  return currentItem?.label ?? 'Dashboard';
+  logger(currentItem, "currentItem");
+  return currentItem?.label ?? "Dashboard";
 }
